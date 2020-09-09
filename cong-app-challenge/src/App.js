ebe6fbee6f82615c7b7206d50f7cee271180c42c
krits29 <31025768+krits29@users.noreply.github.com>
2020-09-08 22:18:00 -0700

    Guests added

--- cong-app-challenge/src/App.js
+++ cong-app-challenge/src/App.js
@@ -4,7 +4,7 @@ import Event from "./Event/Event";
 import { BrowserRouter } from "react-router-dom";
 import { Route } from "react-router-dom";
 import EventDashboard from './EventDashboard/EventDashboard';
-
+import Guest from './Guest/Guest.js';
 
 class App extends Component {
   render() {
@@ -12,6 +12,24 @@ class App extends Component {
       <BrowserRouter>
         <div>
           <Route path = "/" exact component = {EventDashboard}/>
+          <div className="App">
+            <header className="App-header">
+              <h1 className="App-title">Party Planner</h1>
+              <h2>Guest List</h2>
+            </header>
+            <h1 className="body">Guest Count</h1>
+            <div className="row">
+              <p className="column">Name</p>
+              <p className="column">Email</p>
+              <p className="column">Status</p>
+            </div>
+            <Guest guestName="Jane" guestEmail="jane@gmail.com" guestStatus="Accepted"/>
+            <Guest guestName="Nancy" guestEmail="nancy@gmail.com" guestStatus="Accepted"/>
+            <Guest guestName="Jooe" guestEmail="jooe@gmail.com" guestStatus="Pending"/>
+            <Guest guestName="Pablo" guestEmail="pablo@yahoo.com" guestStatus="Accepted"/>
+            <Guest guestName="Bree" guestEmail="bree@gmail.com" guestStatus="Pending"/>
+            <button>+New Guest</button>
+          </div>
         </div>
       </BrowserRouter>
     );

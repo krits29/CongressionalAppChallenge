92f18591e770a97b5cc6286a73756b2bbc9a5e35
krits29 <31025768+krits29@users.noreply.github.com>
2020-09-08 22:03:15 -0700

    Event Dashboard

--- cong-app-challenge/src/App.js
+++ cong-app-challenge/src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import Event from "./Event/Event";
+import { BrowserRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
+import EventDashboard from './EventDashboard/EventDashboard';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Party Planner</h1>
-          <h2>Event Dashboard</h2>
-        </header>
-        <h1 className="body">My Events</h1>
-        <div className="row">
-          <p className="column">Event</p>
-          <p className="column">Time</p>
+      <BrowserRouter>
+        <div>
+          <Route path = "/" exact component = {EventDashboard}/>
         </div>
-        <Event eventName="Sally's Bday" eventTime="1:00pm"/>
-        <Event eventName="Casie's Baby Shower" eventTime="4:00pm"/>
-        <Event eventName="Nellie's Graduation" eventTime="7:00pm"/>
-        <button>+Add New Event</button>
-      </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ cong-app-challenge/src/EventDashboard/EventDashboard.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Event from "../Event/Event";
+
+class EventDashboard extends Component {
+    render() {
+        return(
+            <div className="App">
+            <header className="App-header">
+              <h1 className="App-title">Party Planner</h1>
+              <h2>Event Dashboard</h2>
+            </header>
+            <h1 className="body">My Events</h1>
+            <div className="row">
+              <p className="column">Event</p>
+              <p className="column">Time</p>
+            </div>
+            <Event eventName="Sally's Bday" eventTime="1:00pm"/>
+            <Event eventName="Casie's Baby Shower" eventTime="4:00pm"/>
+            <Event eventName="Nellie's Graduation" eventTime="7:00pm"/>
+            <button>+Add New Event</button>
+          </div>
+        );
+    }
+}
+
+export default EventDashboard;
